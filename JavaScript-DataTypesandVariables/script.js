28177473b9016c65655305c824cf357e77f068d4
tomihristov5 <hrist0v@abv.bg>
2015-05-27 17:54:17 +0300

    final

---
+++ JavaScript-DataTypesandVariables/script.js
@@ -0,0 +1,35 @@
+﻿// Problem 1
+console.log('Problem 1');
+var a = 1,
+    b = 13.65,
+    c = false,
+    d = 'javascript',
+    e = [1, 3, 5, 10],
+    f = { firstName: 'pesho', lastName: 'goshov' }
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
+
+// Problem 2
+console.log('Problem 2');
+var quote = '\'How you doin\'?\', Joey said\'.';
+console.log(quote);
+
+// Problem 3
+console.log('Problem 3');
+console.log(typeof(a));
+console.log(typeof(b));
+console.log(typeof(c));
+console.log(typeof(d));
+console.log(typeof(e));
+console.log(typeof (f));
+
+// Problem 4
+console.log('Problem 4');
+var un,
+    n = null;
+console.log(typeof(un));
+console.log(typeof (n));
